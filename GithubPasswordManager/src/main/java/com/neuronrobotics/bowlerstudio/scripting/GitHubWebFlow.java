9ab1f055a1e75a3d1c7f110c10c7631384ac5fd7
Kevin Harrington <mad.hephaestus@gmail.com>
2020-05-17 10:25:11 -0400

    Attempt to build a proof of concept for issue
    https://github.com/hub4j/github-api/issues/789
    
    I will attempt to demonstrate the functionality needed without changing
    the core API.

---
+++ GithubPasswordManager/src/main/java/com/neuronrobotics/bowlerstudio/scripting/GitHubWebFlow.java
@@ -0,0 +1,17 @@
+package com.neuronrobotics.bowlerstudio.scripting;
+
+public class GitHubWebFlow implements IGitHubLoginManager {
+
+	@Override
+	public String[] prompt(String loginID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String twoFactorAuthCodePrompt() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
